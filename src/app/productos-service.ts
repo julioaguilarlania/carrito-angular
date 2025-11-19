46b1dbc4ef0ddf8b8b37f0415c37c5a23a68a13e
Julio Cesar Aguilar Cabrera <julio.aguilar@lania.edu.mx>
2025-11-19 15:06:01 -0600

    Agrega servicio de Productos

---
+++ src/app/productos-service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Producto } from './productos/producto.model';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductosService {
+  
+  urlProductos = "http://localhost:8080/api/productos";
+
+  constructor(private http:HttpClient) {}
+
+  getProductos() : Observable<Producto[]> {
+    return this.http.get<Producto[]>(this.urlProductos);
+  }
+}
