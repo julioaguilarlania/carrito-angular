e58d80237980be39fdf07fc57cd041cfe533ca65
Julio Cesar Aguilar Cabrera <julio.aguilar@lania.edu.mx>
2025-11-21 15:08:59 -0600

    Implementa ejemplo de uso de NGXLogger

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,21 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { LoggerModule, NgxLoggerLevel } from 'ngx-logger';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),
+    importProvidersFrom(LoggerModule.forRoot({
+      level: NgxLoggerLevel.DEBUG,
+      //disableConsoleLogging: true
+      //serverLogLevel: NgxLoggerLevel.ERROR,
+      //serverLoggingUrl: 'http://localhost:8080/api/logs'
+    }))
   ]
 };
--- src/app/productos-service.ts
+++ src/app/productos-service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Producto } from './productos/producto.model';
 import { Observable } from 'rxjs';
+import { NGXLogger } from 'ngx-logger';
 
 @Injectable({
   providedIn: 'root',
@@ -10,13 +11,17 @@ export class ProductosService {
   
   urlProductos = "http://localhost:8080/api/productos";
 
-  constructor(private http:HttpClient) {}
+  constructor(private http:HttpClient, private logger: NGXLogger) {}
 
   getProductos() : Observable<Producto[]> {
-    return this.http.get<Producto[]>(this.urlProductos);
+    this.logger.info("getProductos()");
+    var resultado = this.http.get<Producto[]>(this.urlProductos);
+    this.logger.trace("resultado de consulta", resultado);
+    return resultado;
   }
 
   crearProducto(prod: Producto) : Observable<Producto> {
+    this.logger.info("crearProducto()");
     return this.http.post<Producto>(this.urlProductos, prod);
   }
 }
