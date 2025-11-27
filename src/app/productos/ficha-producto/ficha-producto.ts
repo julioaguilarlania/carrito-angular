f1cc17de5578b35810508a0eda96a7b7aa8e953a
Julio César Aguilar <julio.aguilar@lania.edu.mx>
2025-11-27 13:14:44 -0600

    Modifca ficha producto para comunicarse con carrito

--- src/app/productos/ficha-producto/ficha-producto.ts
+++ src/app/productos/ficha-producto/ficha-producto.ts
@@ -1,13 +1,22 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Producto } from '../producto.model';
+import { CurrencyPipe } from '@angular/common';
+import { ComunicacionService } from '../../carrito/comunicacion-service';
 
 @Component({
   selector: 'app-ficha-producto',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './ficha-producto.html',
   styleUrl: './ficha-producto.scss',
 })
 export class FichaProducto {
 
   @Input() producto!: Producto;
+
+  constructor(private comService: ComunicacionService) { }
+
+  agregar(prod: Producto) {
+    this.comService.enviarProducto(prod);
+  }
+
 }
